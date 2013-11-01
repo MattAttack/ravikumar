585c2907fe0e3537f67f1eea729b932526078fd8
Jay Shah <jayshahtx@gmail.com>
2013-11-01 18:46:03 -0500

    added email support
    Had trouble importing and using "email class" - received an error on
    importing "Parser" method. Will have to play around with "email" class
    later on. For now, emails are printing

--- calendar.py
+++ calendar.py
@@ -8,15 +8,26 @@ import gdata.calendar.client
 import gdata.acl.data
 import atom.data
 import time
+#imports used for email connection
+import imaplib
+import email
+
 
 #global variables used for authentication
 client = gdata.calendar.client.CalendarClient(source='Where\'s A-wheres-a-v1')
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
 username = 'utcaldummy'
 password = 'utcaldummy123'
 
-# Authenticate for the calendar
+# Authenticate for the calendar and email
 def createConnection():
+	#conncet to calendar
 	client.ClientLogin(username, password, client.source) 
+	#connect to email inbox
+	mail.login(username+'@gmail.com', password)
+	mail.list()
+	mail.select("INBOX") # connect to inbox.
+	print "Succesfully Connected to Calendar and Inbox \n"
 
 def setUpCalendars():
 	# Figure out what calendars that user has
@@ -40,6 +51,18 @@ def setUpCalendars():
 	for i, an_event in enumerate(feed.entry):
 		print '\t%s. %s' % (i, an_event.title.text)
 
+def accessEmail():
+	status, data = mail.search(None, 'ALL')
+	ids = data[0] # data is a list.
+	id_list = ids.split() # ids is a space separated string
+	latest_email_id = id_list[-1] # get the latest
+	result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID	 
+	raw_email = data[0][1] #this variable is an "email" object
+	print "Printing 1st Read/Unread Email: \n"
+	print raw_email
+
+
 
 createConnection()
 setUpCalendars()
+accessEmail()

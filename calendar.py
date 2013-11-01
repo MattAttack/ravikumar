9bb26274b2b1dd3efcc8f4fcab6a4403715f92a8
Jay Shah <jayshahtx@gmail.com>
2013-11-01 18:21:58 -0500

    created functions

--- calendar.py
+++ calendar.py
@@ -9,28 +9,37 @@ import gdata.acl.data
 import atom.data
 import time
 
+#global variables used for authentication
+client = gdata.calendar.client.CalendarClient(source='Where\'s A-wheres-a-v1')
+username = 'utcaldummy'
+password = 'utcaldummy123'
 
 # Authenticate for the calendar
-client = gdata.calendar.client.CalendarClient(source='Where\'s A-wheres-a-v1')
-client.ClientLogin('utcaldummy', 'utcaldummy123', client.source) 
+def createConnection():
+	client.ClientLogin(username, password, client.source) 
 
-# Figure out what calendars that user has
-feed = client.GetAllCalendarsFeed()
-print feed.title.text
+def setUpCalendars():
+	# Figure out what calendars that user has
+	feed = client.GetAllCalendarsFeed()
+	print feed.title.text
 
-# Grab that user's calendars
-for i, a_calendar in enumerate(feed.entry):
-  print '\t%s. %s' % (i, a_calendar.title.text)
+	# Grab that user's calendars
+	for i, a_calendar in enumerate(feed.entry):
+  		print '\t%s. %s' % (i, a_calendar.title.text)
 
-# Get calendar event's on a specific day/range
-start_date = '2013-10-31'
-end_date = '2013-11-29'
+	# Get calendar event's on a specific day/range
+	start_date = '2013-10-31'
+	end_date = '2013-11-29'
 
-query = gdata.calendar.client.CalendarEventQuery()
-query.start_min = start_date
-query.start_max = end_date
+	query = gdata.calendar.client.CalendarEventQuery()
+	query.start_min = start_date
+	query.start_max = end_date
 
-print 'Grabbing events between %s -- %s' % (start_date, end_date)
-feed = client.GetCalendarEventFeed(q=query)
-for i, an_event in enumerate(feed.entry):
-  print '\t%s. %s' % (i, an_event.title.text)
+	print 'Grabbing events between %s -- %s' % (start_date, end_date)
+	feed = client.GetCalendarEventFeed(q=query)
+	for i, an_event in enumerate(feed.entry):
+		print '\t%s. %s' % (i, an_event.title.text)
+
+
+createConnection()
+setUpCalendars()

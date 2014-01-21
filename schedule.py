74899f738f28c033050c8a96e01bc7b2c9d57798
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-01-21 15:36:46 -0600

    added insert single event and some documentation, no parse support yet so still using quickadd:

--- schedule.py
+++ schedule.py
@@ -74,6 +74,40 @@ def initiate_loop():
     # Send the request and receive the response
     new_event = client.InsertEvent(event)
 
+def parse():
+    # Parse is currently undefined by the different parameters that could be parsed for are:
+    # - title, conent, where, start_time, end_time. These will then be passed in order to insert a single
+    # event. RIght now there is currently no parse support for this.
+    return
+
+# Found here: https://developers.google.com/google-apps/calendar/v2/developers_guide_python#CreatingSingle
+#
+# Not currently used, but should be used once we better understand how to parse the information.
+def InsertSingleEvent(calendar_client=client,
+                                title=None,
+                                content=None,
+                                where=None,
+                                start_time=None,
+                                end_time=  None):
+    event = gdata.calendar.data.CalendarEventEntry()
+    event.title = atom.data.Title(text=title)
+    event.content = atom.data.Content(text=content)
+    event.where.append(gdata.calendar.data.CalendarWhere(value=where))
+
+    if start_time is None:
+      # Use current time for the start_time and have the event last 1 hour
+      start_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime())
+      end_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(time.time() + 3600))
+    event.when.append(gdata.calendar.data.When(start=start_time, end=end_time))
+
+    new_event = calendar_client.InsertEvent(event)
+
+    print 'New single event inserted: %s' % (new_event.id.text,)
+    print '\tEvent edit URL: %s' % (new_event.GetEditLink().href,)
+    print '\tEvent HTML URL: %s' % (new_event.GetHtmlLink().href,)
+
+    return new_event
+
 def main():
     create_connection()
     set_up_calendars()

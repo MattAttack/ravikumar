0f67383ca1df5a4fb23400f459df93c9af24842d
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-02-27 17:35:48 -0600

    refactoring

--- schedule.py
+++ schedule.py
@@ -41,13 +41,13 @@ def create_connection():
     client.ClientLogin(username, password, client.source)
 
     # Connect to email inbox
-    mail.login(username+'@gmail.com', password)
+    mail.login(username + '@gmail.com', password)
     mail.list()
     mail.select("INBOX")        # Connect to the inbox
 
     print "Succesfully Connected to Calendar and Inbox! \n"
 
-#retrieves email
+# Retrieves the move recent email
 def check_email():
     email_text = pull_email.getEmail()
     print "Email text: \n" + email_text
@@ -81,39 +81,8 @@ def parse_email(email_body):
     print "Parsed entities: Y:%s M:%s D:%s H:%s M:%s S:%s" % (t[0], t[1], t[2], t[3], t[4], t[5])
     return t
 
-
-
-
-# check_availability comments..
-def check_availability(start_date, end_date):
-    query = gdata.calendar.client.CalendarEventQuery()
-    query.start_min = start_date
-    query.start_max = end_date
-
-    print 'Grabbing events between %s -- %s' % (start_date, end_date)
-    feed = client.GetCalendarEventFeed(q=query)
-    # for i, an_event in enumerate(feed.entry):
-    #     print '\t%s. %s' % (i, an_event.title.text)
-    #     print 'Begins at %s' % (str(an_event.when[0]))
-
-
-# The loop that takes in an input and parses it and then executes the appropraite event
-# Currently called 'initiate loop', needs a better name but should work for basic implementation
-def manual_input():
-    user_input = raw_input("Enter event title, event will automatically be made for right now: ")
-
-    # Create the event. Currently using Google's quick add feature which handles the extensive
-    # parsing for you. Information on how to use this can be found here:
-    # https://developers.google.com/google-apps/calendar/v2/developers_guide_python#CreatingSingle
-    event = gdata.calendar.data.CalendarEventEntry()
-    event.content = atom.data.Content(text=user_input)
-    event.quick_add = gdata.calendar.data.QuickAddProperty(value='true')
-
-    # Send the request and receive the response
-    new_event = client.InsertEvent(event)
-
-
-# Check for calendar conflicts.
+# Checks for calendar conflicts between the start date and the end date
+# Returns a list of those conflicts
 def findConflicts(start_date, end_date):
     # Construct the calendar query
     query = gdata.calendar.client.CalendarEventQuery()
@@ -122,19 +91,15 @@ def findConflicts(start_date, end_date):
 
     print
     print 'Grabbing events between %s -- %s' % (start_date, end_date)
-    feed = client.GetCalendarEventFeed(q=query)
+    feed = client.GetCalendarEventFeed(q=query) # Execute the query
     for i, an_event in enumerate(feed.entry):   # Go over the calendar and pring out the events
         print '\t%s. %s' % (i, an_event.title.text)
-
-    # TODO: Modify to return the events if needed
     print
 
     return feed.entry
 
 
 # Found here: https://developers.google.com/google-apps/calendar/v2/developers_guide_python#CreatingSingle
-#
-# Not currently used, but should be used once we better understand how to parse the information.
 def InsertSingleEvent(calendar_client=client,
                                 title=None,
                                 content=None,
@@ -158,11 +123,12 @@ def InsertSingleEvent(calendar_client=client,
     return new_event
 
 # Takes the entities and constructs them appropriately in the format
-# google's API accepts and likes
+# google's API accepts and likes.
+# TODO: Currently the timezone is hardcoded in, should eventually be fixed
 def time_object(year, month, day, hour, minute, second):
-    month = "%02d" % int(month)
-    day = "%02d" % int(day)
-    hour = "%02d" % int(hour)
+    month  = "%02d" % int(month)
+    day    = "%02d" % int(day)
+    hour   = "%02d" % int(hour)
     minute = "%02d" % int(minute)
     second = "%02d" % int(second)
 
@@ -174,23 +140,26 @@ def prompt_user_for_time(parsed, duration, email_body):
     # Load saved information from past decision
     past_decisions = pickle.load(open("save.p", "rb"))
 
-
+    # Construct string for day start/end for Google's Cal API
     day_start_str = time_object(parsed[0][0], parsed[1][0], parsed[2][0], 0, 0, 0)
     day_end_str   = time_object(parsed[0][0], parsed[1][0], parsed[2][0], 23, 59, 59)
+
+    # Construct the time obejct for simple comparisons
     day_start_time = parser.parse(day_start_str)
     day_end_time = parser.parse(day_end_str)
 
     possible_times = []             # Generate a list of all the possible times that day
     while (day_start_time < day_end_time):
-        possible_time = (day_start_time, day_start_time + datetime.timedelta(minutes=duration))   # Duration will be an hour
+        possible_time = (day_start_time, day_start_time + datetime.timedelta(minutes=duration))
         possible_times.append(possible_time)
-        day_start_time = day_start_time + datetime.timedelta(minutes=30)
+        day_start_time = day_start_time + datetime.timedelta(minutes=30)    # Times currently generated on the half hour
 
     # Filter out the times that are not valid
     for e_conflict in findConflicts(day_start_str, day_end_str):
         conflict_start = parser.parse(e_conflict.when[0].start)
         conflict_end   = parser.parse(e_conflict.when[0].end)
 
+        # Filter out the bad times for that event
         possible_times = filter(lambda time: (time[1] <= conflict_start or conflict_end <= time[0]), possible_times)
 
     # Print out the possible times, with an associated index
@@ -206,10 +175,34 @@ def prompt_user_for_time(parsed, duration, email_body):
     # Save the new decision. Saved in (choices, decision, email_body) triples.
     new_decision = (possible_times, possible_times[user_selection], email_body)
     past_decisions.append(new_decision)
-    pickle.dump(past_decisions, open("save.p", "wb"))
+
+    pickle.dump(past_decisions, open("save.p", "wb"))   # Save the past decisions
+                                                        # with the new one
 
     return possible_times[user_selection]
 
+# Schedule event based on the parsed infromation and the email body
+def schedule_event(email_body, parsed):
+    event_name = raw_input('Name of the event: ')                           # The event needs some name
+    event_duration = int(raw_input('Duration of the event (minutes): '))    # and a duration
+
+    print "Scheduling an event now.."
+    if len(parsed[3]) == 1:     # Unclear hour, this assumes definite date TODO: was == 1 while regex would always return something
+        possible_time = prompt_user_for_time(parsed, event_duration, email_body)    # Grab the time the user wants
+
+        # Schedule at the time requested by the users
+        InsertSingleEvent(client,
+                          event_name,
+                          None,
+                          None,
+                          possible_time[0].strftime("%Y-%m-%dT%H:%M:%S") + "-06:00", # This is hacky. %:z gives me what I want
+                          possible_time[1].strftime("%Y-%m-%dT%H:%M:%S") + "-06:00"  # but I was unable to get it to work
+                          )
+    else:           # Otherwise schedule, defaults to a duration of 1 hour
+        start_time = time_object(parsed[0][0], parsed[1][0], parsed[2][0], parsed[3][0], parsed[4][0], parsed[5][0])
+        # TODO: Have end time incorporate the duration
+        end_time = time_object(parsed[0][0], parsed[1][0], parsed[2][0], str(int(parsed[3][0]) + 1), parsed[4][0], parsed[5][0])
+        InsertSingleEvent(client, event_name, None, None, start_time, end_time)
 
 def main():
     create_connection()                 # need to connect to calendar now
@@ -218,25 +211,9 @@ def main():
 
     # Prompt the user to schedule an event
     should_schedule = raw_input("Would you like to schedule an event? ")
-    if (should_schedule[0] == 'y' or should_schedule[0] == 'Y'):
-        event_name = raw_input('Name of the event: ')       # The event needs some name
-        event_duration = int(raw_input('Duration of the event (minutes): '))
 
-        print "Scheduling an event now.."
-        if len(parsed[3]) == 1:     # Unclear hour, this assumes definite date
-            possible_time = prompt_user_for_time(parsed, event_duration, email_body)
-            pdb.set_trace()
-            InsertSingleEvent(client,
-                              event_name,
-                              None,
-                              None,
-                              possible_time[0].strftime("%Y-%m-%dT%H:%M:%S") + "-06:00", # This is hacky. %:z gives me what I want
-                              possible_time[1].strftime("%Y-%m-%dT%H:%M:%S") + "-06:00"  # but I was unable to get it to work
-                              )
-        else:           # Otherwise schedule, defaults to a duration of 1 hour
-            start_time = time_object(parsed[0][0], parsed[1][0], parsed[2][0], parsed[3][0], parsed[4][0], parsed[5][0])
-            end_time = time_object(parsed[0][0], parsed[1][0], parsed[2][0], str(int(parsed[3][0]) + 1), parsed[4][0], parsed[5][0])
-            InsertSingleEvent(client, event_name, None, None, start_time, end_time)
+    if (should_schedule[0] == 'y' or should_schedule[0] == 'Y'):
+        schedule_event(email_body, parsed)
 
 if __name__ == "__main__":
     main()

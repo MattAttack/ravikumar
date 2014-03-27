f258549f0c8ccec909ce9fafb1fd2556cff056d5
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-03-26 21:31:01 -0500

    more magic

--- skeleton.py
+++ skeleton.py
@@ -82,37 +82,85 @@ def log_updates():
     save("seen_emails.p", seen_emails)
     save("output_log.p", output_log)
 
+def time_object(year, month, day, hour, minute, second):
+    month  = "%02d" % int(month)
+    day    = "%02d" % int(day)
+    hour   = "%02d" % int(hour)
+    minute = "%02d" % int(minute)
+    second = "%02d" % int(second)
+
+    return '%s-%s-%sT%s:%s:%s-06:00' % (year, month, day, hour, minute, second)
 
 def parse_email(email_body):
     days = get_possible_days(email)
-    times = get_possible_times(day)
+    possible_times = get_possible_times()
 
     # Create a list of days and times
-    possible_times = [] # [(day, time)]
-
+    possible_times = combine_days_with_possible_times(days)
     free_times = filter_out_conflicts(possible_times)
 
-    return free_times # Already ranked
+    # TODO: Rank the times
+
+    return free_times
 
     # TODO: Jay
+    # Note: Jay for possible days have this return a list of
+    # [(year, month, date)] where each is an int. This is what I will
+    # be expecting
     def get_possible_days(email_body):
         # Return a list of the possible days
         pass
 
-    # TODO: Matt
-    def get_possible_times():
-        # Generate all of the times and rank them
-        pass
+    def get_possible_times(possible_days):
+        possible_times = []         # Stores tuples (start_time, end_time)
+        duration = 60               # Currently duration of event is an hour
 
-    # TODO: Matt
-    def filter_out_conflicts(possible_times):
-        # Filter out all of the times with conflicts
-        pass
+        # Generate all the possible times for all the possible days
+        for possible_day in possible_days:
+            day_start_str = time_object(possible_day[0], possible_day[1],
+                                        possible_day[2], 0, 0, 0)
+            day_end_str   = time_object(possible_day[0], possible_day[1],
+                                        possible_day[2], 23, 59, 59)
 
-# TODO: Matt
+            day_start_time = parser.parse(day_start_str)
+            day_end_time   = parser.parse(day_end_str)
+
+            while(day_start_time < day_end_time):
+                with end_time as day_start_time + datetime.timedelta(minutes=duration):
+                    new_possible_time = (day_start_time, end_time)
+                possible_times.append(new_possible_time)
+                day_start_time += datetime.timedelta(minutes=30)
+
+            # Faster to filter out conflicts on per day basis than at the end
+            for e_conflict in findConflicts(day_start_str, day_end_str):
+                conflict_start = parser.parse(e_conflict.when[0].start)
+                conflict_end   = parser.parse(e_conflict.when[0].end)
+
+                # Filter out the bad times for that event
+                possible_times = filter(lambda time: (time[1] <= conflict_start or conflict_end <= time[0]), possible_times)
+
+        return possible_times
+
+    def findConflicts(start_date, end_date):
+        # Construct the calendar query
+        query = gdata.calendar.client.CalendarEventQuery()
+        query.start_min = start_date
+        query.start_max = end_date
+
+        feed = client.GetCalendarEventFeed(q=query) # Execute the query
+
+        return feed.entry
+
 def prompt_user(times):
-    # Prompt the user for a time
-    pass
+    # Print out the possible times, with an associated index
+    print 'Please select a start time for your event: '
+    for i, possible_time in enumerate(possible_times):
+        print '%02d :: %s - %s' % (i, possible_time[0].strftime("%a %m-%d %I:%M%p"), possible_time[1].strftime("%I:%M%p"))
+
+
+    print               # Prompt user for a selection
+    user_selection = int(input("Please select the most optimal time: "))
+    return user_selection
 
 def check_for_new_emails_and_prompt():
     status, data = mail.search(None, 'ALL')     # Grab all the emails
@@ -148,9 +196,13 @@ def process_email(subject, body, sender):
     print "From: %s" % sender
     print "%s" % body
 
-    # possible_times = parse_email(body)
-    # user_selection = prompt_user(possible_times)
+    possible_times = parse_email(body)
+    user_selection = prompt_user(possible_times)
 
+    # TODO: Append that body to the appropriate time vector
+    # TODO: Log the email accordingly
+
+    seen_emails.append(subject, body)
 
 def main():
     create_connection()

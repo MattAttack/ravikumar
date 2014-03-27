cd7c34143dc5976f763b724960757bfe50345a66
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-03-26 20:44:14 -0500

    added looping through all of the newest emails

--- skeleton.py
+++ skeleton.py
@@ -39,6 +39,7 @@ mail = imaplib.IMAP4_SSL('imap.gmail.com')
 username = 'utcaldummy'
 password = 'utcaldummy123'
 
+
 # Authenticate for the calendar and email to be able to access the user's
 # email and access the calendarb
 def create_connection():
@@ -47,36 +48,29 @@ def create_connection():
 
     mail.login(username+'@gmail.com', password)
     mail.list()
-    mail.select("INBOX") # connect to inbox.
+    mail.select("INBOX")
 
     print 'Successfully connected to email and calendar!'
 
+
 def load_variables():
     global seen_emails, time_vectors, output_log
-    time_vectors = load("time_vectors.p",time_vectors)
 
-    # loads data if it already exists, creates pickle file otherwise
-    def load(fileName,data):
-        #check if it exists
-        if os.path.isfile(fileName):
-            with open(fileName,'wb') as f:
-                pickle.dump(variable,f)
-            return data
-        #create if it does not
-        else:
-            with open(fileName,'rb') as f:
-                data = pickle.load(f)
-            return data
+    # Loads data if it already exists, creates pickle file otherwise
+    def load(fileName, data):
+        # If the file doesn't exists, make a new file for it
+        if not os.path.isfile(fileName):
+            with open(fileName, 'wb') as f:
+                pickle.dump(data, f)
 
-    # TODO: Load previous data:
-    #   - seen emails
-    #   - time vectors
-    #   - log stuff
+        # Return the data from the file
+        with open(fileName,'rb') as f:
+                return pickle.load(f)
 
-# Returns the body of the user's most recent email
-# TODO: Matt
-def get_most_recent_emails():
-    return []
+    # Load up each of the values needed
+    time_vectors = load("time_vectors.p", time_vectors)
+    #   seen emails
+    #   log stuff
 
 def parse_email(email_body):
     days = get_possible_days(email)
@@ -116,17 +110,43 @@ def log_updates():
     # Update output_log
     pass
 
+def check_for_new_emails_and_prompt():
+    status, data = mail.search(None, 'ALL')     # Grab all the emails
+    email_ids = data[0].split()                 # and their email ids
+
+    # Scan the list from new to old.
+    for i in xrange(len(email_ids) - 1, 0, -1):
+        email_id = email_ids[i]             # Fetch that email
+        result, data = mail.fetch(email_id, "(RFC822)")
+
+        raw_email = data[0][1]              # Turn it into an email object
+        email_obj = email.message_from_string(raw_email)
+
+        # Payload can either be a list (HTML & Reg Version), or just Reg
+        if isinstance(em._payload, list):
+            body = email_obj._payload[0]._payload
+        else:
+            body = email_obj._payload
+
+        subj   = email_obj["Subject"]
+        sender = email_obj["From"]
+
+        # Seen this email before? -> Seen all older. Terminate
+        if (subj, body) in seen_emails:
+            return
+
+        # If you haven't seen this before handle accordingly
+        process_email(subj, body, sender)
+        pdb.set_trace()
+
+def process_email(subject, body, sender):
+
+    pass
+
 def main():
     create_connection()
-    load_variables()                        # TODO: Both
-
-    # Get all new emails
-    # TODO: Matt: Move this to a method, main method should be clean
-    new_emails = get_most_recent_emails()
-    for email in new_emails:
-        times = parse_email(email)
-        user_selection = prompt_user(times)
-        log_updates()
+    load_variables()
+    check_for_new_emails_and_prompt()
 
 if __name__ == '__main__':
     main()
